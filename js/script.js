d773348d28d7e7a76661c1882de15994425fa8b4
JunHinata <67004258+JunHinata@users.noreply.github.com>
2021-12-05 21:14:02 +0900

    first commit
    ポートフォリオ-トップサイト

---
+++ js/script.js
@@ -0,0 +1,71 @@
+/* 
+#photo-gallery
+=========================================== */
+$('#photo-gallery').hover(
+  function() {
+    // マウスホバー時
+    $('#bg-white').addClass('photo-hover');
+    $('#bg-red').addClass('photo-hover');
+    $('#bg-blue').addClass('photo-hover');
+  },
+  function() {
+    // マウスアウト時
+    $('#bg-white').removeClass('photo-hover');
+    $('#bg-red').removeClass('photo-hover');
+    $('#bg-blue').removeClass('photo-hover');
+  }
+);
+
+/* 
+#landing-page
+=========================================== */
+$('#landing-page').hover(
+  function() {
+    // マウスホバー時
+    $('#bg-white').addClass('lp-hover');
+    $('#bg-red').addClass('lp-hover');
+    $('#bg-blue').addClass('lp-hover');
+  },
+  function() {
+    // マウスアウト時
+    $('#bg-white').removeClass('lp-hover');
+    $('#bg-red').removeClass('lp-hover');
+    $('#bg-blue').removeClass('lp-hover');
+  }
+);
+
+/* 
+#corporate-site
+=========================================== */
+$('#corporate-site').hover(
+  function() {
+    // マウスホバー時
+    $('#bg-white').addClass('corp-hover');
+    $('#bg-red').addClass('corp-hover');
+    $('#bg-blue').addClass('corp-hover');
+  },
+  function() {
+    // マウスアウト時
+    $('#bg-white').removeClass('corp-hover');
+    $('#bg-red').removeClass('corp-hover');
+    $('#bg-blue').removeClass('corp-hover');
+  }
+);
+
+/* 
+#cocktail-recipe-app
+=========================================== */
+$('#cocktail-recipe-app').hover(
+  function() {
+    // マウスホバー時
+    $('#bg-white').addClass('cocktail-hover');
+    $('#bg-red').addClass('cocktail-hover');
+    $('#bg-blue').addClass('cocktail-hover');
+  },
+  function() {
+    // マウスアウト時
+    $('#bg-white').removeClass('cocktail-hover');
+    $('#bg-red').removeClass('cocktail-hover');
+    $('#bg-blue').removeClass('cocktail-hover');
+  }
+);
